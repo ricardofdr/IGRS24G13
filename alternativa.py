e77d21c3c32be9b9bd212c6cad6d98c506ae619d
JohanneFurtado <apollonmonteiro@gmail.com>
2024-12-20 20:40:13 +0000

    organizacao

--- alternativa.py
+++ alternativa.py
@@ -29,6 +29,7 @@ class kamailio:
     def set_state(self, user, state):
         user_states[user] = state
         name = {0: "Registed", 1: "Call", 2: "Conference"}.get(state, "Unkwon")
+        KSR.hdr.add("State {user}", str(name))
         KSR.info(f"State {user}: {name} ({state})\n")
     
     def get_state(self, user):
@@ -37,6 +38,7 @@ class kamailio:
     def remove_user_state(self, user):
         if user in user_states:
             del user_states[user]
+            KSR.hdr.add("State {user}", str("State {user} removed"))
             KSR.info(f"State {user} removed\n")
 
     def ksr_request_route(self, msg):
@@ -45,21 +47,20 @@ class kamailio:
             KSR.info("REGISTER R-URI: " + KSR.pv.get("$ru") + "\n")
             KSR.info("            To: " + KSR.pv.get("$tu") + " Contact: " + KSR.hdr.get("Contact") + "\n")
             
-            # Reject registration if not @acme.pt
-            if not KSR.pv.get("$tu").endswith("@acme.pt"):
+
+            if  KSR.pv.get("$tu").endswith("@acme.pt"):
+                if KSR.registrar.save('location', 0):
+                    KSR.info("Registration saved for " + KSR.pv.get("$tu") + "\n")
+                    self.set_state(KSR.pv.get("$tu"), 0)
+                    KSR.sl.send_reply(200, "OK")
+                else:
+                    KSR.info("Registration failed for " + KSR.pv.get("$tu") + "\n")
+                    KSR.sl.send_reply(500, "Internal Server Error")
+            else:
                 KSR.info("Registration rejected for non-acme.pt domain: " + KSR.pv.get("$tu") + "\n")
                 KSR.sl.send_reply(403, "Forbidden")
-                return 1
-
-            # Save registration for valid users
-            if KSR.registrar.save('location', 0):
-                KSR.info("Registration saved for " + KSR.pv.get("$tu") + "\n")
-                self.set_state(KSR.pv.get("$tu"), 0)
-                KSR.sl.send_reply(200, "OK")
-            else:
-                KSR.info("Registration failed for " + KSR.pv.get("$tu") + "\n")
-                KSR.sl.send_reply(500, "Internal Server Error")
             return 1
+
 
         # Handle INVITE (simple forwarding logic)
         if (msg.Method == "INVITE"):
@@ -69,52 +70,65 @@ class kamailio:
             # Debugging From Domain
             KSR.info("From Domain ($fd): " + KSR.pv.get("$fd") + "\n")
             
-            # Reject calls from non-acme.pt domains
-            if "acme.pt" not in KSR.pv.get("$fd"):
+            if "acme.pt" in KSR.pv.get("$fd"):
+                
+                #Conference call
+                if "sip:conference@acme.pt" in KSR.pv.get("$tu"):
+                    KSR.pv.sets("$ru", "sip:conference@127.0.0.1:5090")
+                    KSR.info("Calling to Conference. Forwarding call to: " + KSR.pv.get("$ru") + "\n")
+                    self.set_state(KSR.pv.get("$fu"), 2)
+                    KSR.tm.t_relay()
+                
+                # Check if the target user is registered
+                if KSR.registrar.lookup("location") == 1:
+                    KSR.info("User registered. Forwarding call to: " + KSR.pv.get("$ru") + "\n")
+                    
+                    if self.get_state(KSR.pv.get("$tu")) == 1:
+                        KSR.info(KSR.pv.get("$ru") + " is on a call. \n")
+                        KSR.pv.sets("$ru", "sip:busyann@127.0.0.1:5080")
+                        KSR.info("Calling to announcement server. Forwarding call to: " + KSR.pv.get("$ru") + "\n")
+                        self.set_state(KSR.pv.get("$fu"), 1)
+                        KSR.forward() 
+                        KSR.tm.t_relay()
+                        return 1
+                    
+                    elif self.get_state(KSR.pv.get("$tu")) == 2:
+                        KSR.info(KSR.pv.get("$tu") + " is on a Conference.\n")
+                        KSR.pv.sets("$ru", "sip:inconference@127.0.0.1:5080")
+                        KSR.info("Calling to announcement server. Forwarding call to: " + KSR.pv.get("$ru") + "\n")
+                        self.set_state(KSR.pv.get("$fu"), 1)
+                        KSR.forward() 
+                        KSR.tm.t_relay()
+                        
+                        if(msg.Method == "INFO"):
+                            info_body = KSR.textops.get_body()
+                            if "DTMF" in info_body:
+                                dtmf_value = info_body.strip()
+                                if dtmf_value == 0:
+                                    KSR.pv.sets("$ru", "sip:conference@127.0.0.1:5090")
+                                    KSR.info("Calling to Conference. Forwarding call to: " + KSR.pv.get("$ru") + "\n")
+                                    self.set_state(KSR.pv.get("$fu"), 2)
+                                    KSR.tm.t_relay()
+                        return 1
+                    else:
+                        self.set_state(KSR.pv.get("$fu"), 1)
+                        self.set_state(KSR.pv.get("$tu"), 1)
+                        KSR.tm.t_relay()  # Forward the call
+                else:
+                    KSR.info("User not registered: " + KSR.pv.get("$tu") + "\n")
+                    KSR.sl.send_reply(404, "User Not Registered")
+            else:
                 KSR.info("Call rejected from non-acme.pt domain: " + KSR.pv.get("$fd") + "\n")
                 KSR.sl.send_reply(403, "Forbidden")
-                return 1
-            
-            # Conference call
-            if "sip:announce@acme.pt" in KSR.pv.get("$tu"):
-                KSR.pv.sets("$ru", "sip:announce@127.0.0.1:5090")
-                KSR.info("Calling to announce. Forwarding call to: " + KSR.pv.get("$ru") + "\n")
-                self.set_state(KSR.pv.get("$fu"), 2)
-                KSR.tm.t_relay()
-
-            # Check if the target user is registered
-            if KSR.registrar.lookup("location") == 1:
-                KSR.info("User registered. Forwarding call to: " + KSR.pv.get("$ru") + "\n")
-                
-                if self.get_state(KSR.pv.get("$tu")) == 1:
-                    KSR.info(KSR.pv.get("$ru") + " is on a call. \n")
-                    KSR.pv.sets("$ru", "sip:busyann@127.0.0.1:5080")
-                    KSR.info("Calling to announcement server. Forwarding call to: " + KSR.pv.get("$ru") + "\n")
-                    KSR.forward() 
-                    KSR.tm.t_relay()
-                    return 1
-
-                
-                if self.get_state(KSR.pv.get("$tu")) == 2:
-                    KSR.info(KSR.pv.get("$ru") + " is on a Conference.\n")
-                    KSR.pv.sets("$ru", "sip:inconference@127.0.0.1:5080")
-                    KSR.info("Calling to announcement server. Forwarding call to: " + KSR.pv.get("$ru") + "\n")
-                    KSR.forward() 
-                    KSR.tm.t_relay()
-                    return 1
-                self.set_state(KSR.pv.get("$fu"), 1)
-                self.set_state(KSR.pv.get("$tu"), 1)
-                KSR.tm.t_relay()  # Forward the call
-            else:
-                KSR.info("User not registered: " + KSR.pv.get("$tu") + "\n")
-                KSR.sl.send_reply(404, "User Not Registered")
             return 1
+        
+
 
         # Handle BYE (DEREGISTER or call termination)
         if (msg.Method == "BYE"):
             KSR.info("BYE R-URI: " + KSR.pv.get("$ru") + "\n")
-            self.set_state(KSR.pv.get("$tu"), 0)
-            self.set_state(KSR.pv.get("$ru"), 0)
+            #self.set_state(KSR.pv.get("$tu"), 0)
+            #self.set_state(KSR.pv.get("$ru"), 0)
             if KSR.registrar.remove("location", KSR.pv.get("$tu")):
                 KSR.info("Deregistration successful for: " + KSR.pv.get("$tu") + "\n")
                 KSR.sl.send_reply(200, "OK")
